6cda2a66fd2fd737bc56ad76b3c289fad3f51627
Emil Lundberg <emil@yubico.com>
2025-11-05 20:50:38 +0100

    Run tests in setup.sh

--- examples/setup.sh
+++ examples/setup.sh
@@ -7,6 +7,9 @@ git submodule update --init --recursive lib/miracl-core
 if [[ -d lib/mcore ]]; then git -C lib/miracl-core worktree remove --force --force ../mcore ; fi
 git -C lib/miracl-core worktree add ../mcore
 
-cd lib/mcore/rust
-git apply ../../../miracl-alt-compress.patch
-python3 config64.py 31 39
+(cd lib/mcore/rust \
+  && git apply ../../../miracl-alt-compress.patch \
+  && python3 config64.py 31 39 \
+)
+
+cargo test --release
