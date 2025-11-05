d43f00ff4690d5003b86b8a5b0102c1d5e5124d1
Emil Lundberg <emil@yubico.com>
2025-11-05 14:36:23 +0100

    Use Git worktree to silence Git diff status in miracl-core

--- examples/setup.sh
+++ examples/setup.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
+# Exit on error
+set -e
+
 git submodule update --init --recursive lib/miracl-core
-(cd lib/miracl-core/rust && git reset --hard && git clean -dfx . && git apply ../../../miracl-sign.patch && python3 config64.py 31 39)
+if [[ -d lib/mcore ]]; then git -C lib/miracl-core worktree remove --force --force ../mcore ; fi
+git -C lib/miracl-core worktree add ../mcore
+
+cd lib/mcore/rust
+git apply ../../../miracl-sign.patch
+python3 config64.py 31 39
